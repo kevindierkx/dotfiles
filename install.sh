402809dc22d6cc641eb7cdc55fa8cd585b0bd611
Kevin Dierkx <kevin@distortedfusion.com>
2020-08-27 11:18:17 +0200

    Added mackup

--- install.sh
+++ install.sh
@@ -29,3 +29,6 @@ $HOME/.composer/vendor/bin/valet install
 # Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
 rm -rf $HOME/.zshrc
 ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
+
+# Symlink the Mackup config file to the home directory
+ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
